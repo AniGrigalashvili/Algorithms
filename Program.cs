6be69d5cdc9167c894404209f5f8f03ade88d0f0
aniko <anogrigalashvili@gmail.com>
2019-10-26 23:41:53 +0400

    change binary_search

--- Program.cs
+++ Program.cs
@@ -6,32 +6,8 @@ namespace ConsoleApp3
     {
         static void Main(string[] args)
         {
-            int[] array = new int[] { 1, 2, 3, 5, 6, 7, 8, 9, 10 };
-            Console.WriteLine(binary_search(array,20));
-            Console.WriteLine(array.Length);
+            new test().Run();
         }
-        static int binary_search(int[] array, int item)
-        {
-            int low = 0;
-            int high = array.Length - 1;
-            int mid = 0;
-            int guess = 0;
-            int result = 0;
-            while(low <= high)
-            {
-                mid = (low + high)/2;
-                guess = array[mid];
-                if (guess == item)
-                {
-                    result = mid;
-                    if (guess > item)
-                        high = mid - 1;
-                    else
-                        low = mid + 1;
-                }
-                else return -1;
-             }
-            return result;
-        }
+        
     }
 }
---
+++ binary_search.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3
+{
+    class test
+    {
+        public void Run()
+        {
+            int[] array = new int[] { 1, 2, 3, 5, 6, 7, 8, 9, 10 };
+            Console.WriteLine(binary_search(array, 20));
+            Console.WriteLine(array.Length);
+        }
+        public int binary_search(int[] array, int item)
+        {
+            int low = 0;
+            int high = array.Length - 1;
+            int mid = 0;
+            int guess = 0;
+            int result = 0;
+            while (low <= high)
+            {
+                mid = (low + high) / 2;
+                guess = array[mid];
+                if (guess == item)
+                {
+                    result = mid;
+                    if (guess > item)
+                        high = mid - 1;
+                    else
+                        low = mid + 1;
+                }
+                else return -1;
+            }
+            return result;
+        }
+    }
+}
