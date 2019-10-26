beb667a51e717626bb8afd632caeca2e27e975d0
aniko <anogrigalashvili@gmail.com>
2019-10-26 22:05:41 +0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 1, 2, 3, 5, 6, 7, 8, 9, 10 };
+            Console.WriteLine(binary_search(array,20));
+            Console.WriteLine(array.Length);
+        }
+        static int binary_search(int[] array, int item)
+        {
+            int low = 0;
+            int high = array.Length - 1;
+            int mid = 0;
+            int guess = 0;
+            int result = 0;
+            while(low <= high)
+            {
+                mid = (low + high)/2;
+                guess = array[mid];
+                if (guess == item)
+                {
+                    result = mid;
+                    if (guess > item)
+                        high = mid - 1;
+                    else
+                        low = mid + 1;
+                }
+                else return -1;
+             }
+            return result;
+        }
+    }
+}
